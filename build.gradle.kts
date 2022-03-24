90861eb4a14f5832fefc2217acb542bb54e8c3ae
D10NG <D10NGYANG@163.com>
2022-03-24 09:28:37 +0800

    优化正则表达字符串处理内容；

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.github.D10NGYANG"
-version = "1.3.0"
+version = "1.3.1"
 
 repositories {
     mavenCentral()
--- src/main/java/com/d10ng/text/string/StringMatchUtils.kt
+++ src/main/java/com/d10ng/text/string/StringMatchUtils.kt
@@ -2,9 +2,9 @@ package com.d10ng.text.string
 
 /**
  * 利用正则表达式保留字符串
- * @receiver String
- * @param reg Regex
- * @return String
+ * @receiver [String]
+ * @param reg [Regex]
+ * @return [String]
  */
 fun String.keep(reg: Regex): String {
     val ls = reg.findAll(this).map { it.value }
@@ -15,13 +15,20 @@ fun String.keep(reg: Regex): String {
 
 /**
  * 利用正则表达式过滤字符串
- * @receiver String
- * @param reg Regex
- * @return String
+ * @receiver [String]
+ * @param reg [Regex]
+ * @return [String]
  */
 fun String.filter(reg: Regex): String {
     return reg.replace(this, "")
 }
+
+/** 数字正则表达式 */
+val numberRegex = "[0-9]+".toRegex()
+/** 字母正则表达式 */
+val letterRegex = "[a-zA-Z]+".toRegex()
+/** 中文正则表达式 */
+val chineseRegex = "[\u4E00-\u9FA5]+".toRegex()
 
 /**
  * 判断字符串是否符合身份证规则
@@ -56,12 +63,28 @@ fun String.isEmail(): Boolean = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]
 fun String.isOnlyChEnNum(): Boolean = "[a-zA-Z0-9\u4E00-\u9FA5]+".toRegex().matches(this)
 
 /**
+ * 判断字符串是否只有英文数字，不包含特殊字符
+ *
+ * @receiver [String]
+ * @return [Boolean]
+ */
+fun String.isOnlyEnNum(): Boolean = "[a-zA-Z0-9]+".toRegex().matches(this)
+
+/**
+ * 判断字符串是否只有中英文，不包含特殊字符
+ *
+ * @receiver [String]
+ * @return [Boolean]
+ */
+fun String.isOnlyChEn(): Boolean = "[a-zA-Z\u4E00-\u9FA5]+".toRegex().matches(this)
+
+/**
  * 判断字符串是否只有中文
  *
  * @receiver [String]
  * @return [Boolean]
  */
-fun String.isOnlyChinese(): Boolean = "[\u4E00-\u9FA5]+".toRegex().matches(this)
+fun String.isOnlyChinese(): Boolean = chineseRegex.matches(this)
 
 /**
  * 判断字符串是否只有英文
@@ -69,7 +92,7 @@ fun String.isOnlyChinese(): Boolean = "[\u4E00-\u9FA5]+".toRegex().matches(this)
  * @receiver [String]
  * @return [Boolean]
  */
-fun String.isOnlyEnglish(): Boolean = "[a-zA-Z]+".toRegex().matches(this)
+fun String.isOnlyEnglish(): Boolean = letterRegex.matches(this)
 
 /**
  * 判断字符串是否只有数字
@@ -77,4 +100,4 @@ fun String.isOnlyEnglish(): Boolean = "[a-zA-Z]+".toRegex().matches(this)
  * @receiver [String]
  * @return [Boolean]
  */
-fun String.isOnlyNumber(): Boolean = "[0-9]+".toRegex().matches(this)
+fun String.isOnlyNumber(): Boolean = numberRegex.matches(this)
